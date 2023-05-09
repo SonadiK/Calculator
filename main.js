c7f92a636d660b609d361191cd091ca6a01f50de
Sonadi Kannangara <ksonadi@gmail.com>
2023-05-09 16:56:09 -0400

    Added comments

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 //
 let input = document.getElementById("input");
 
+// Function to clear the input field.
 function clearInput() {
   input.value = "";
 }
 
+// Function to delete the last character from the input field.
 function backspace() {
   input.value = input.value.slice(0, -1);
 }
 
+// Function to calculate the result of the input expression.
 function calculate() {
   try {
     input.value = eval(input.value);
@@ -17,20 +20,24 @@ function calculate() {
   }
 }
 
+// Function to add a number or operator to the input field.
 function inputNum(num) {
   input.value += num;
 }
 
+// Function to add an operator to the input field.
 function inputOperator(operator) {
   input.value += operator;
 }
 
+// Function to add a decimal point to the input field.
 function inputDecimal() {
   if (!input.value.include(".")) {
     input.value += ".";
   }
 }
 
+// Function to toggle the sign of the input field.
 function inputNegative() {
   if (input.value[0] !== "-") {
     input.value = "-" + input.value;
@@ -38,19 +45,3 @@ function inputNegative() {
     input.value = input.value.slice(1);
   }
 }
-
-// document.addEventListener("mousemove", function (event) {
-//   var cursor = document.querySelector(".cursor");
-//   cursor.style.left = event.clientX + "px";
-//   cursor.style.top = event.clientY + "px";
-// });
-
-// document.addEventListener("mousedown", function () {
-//   var cursor = document.querySelector(".cursor");
-//   cursor.classList.add("mousedown");
-// });
-
-// document.addEventListener("mouseup", function () {
-//   var cursor = document.querySelector(".cursor");
-//   cursor.classList.remove("mousedown");
-// });
